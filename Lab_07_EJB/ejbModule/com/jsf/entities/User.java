9363a678922f5b1edfa6647b7f20ab941b9b6d19
MMyrlak <mikolaj.myrlak@gmail.com>
2023-11-14 11:53:19 +0100

    Lab 07 - EJB, Bazy danych
    Mikołaj Myrlak

---
+++ Lab_07_EJB/ejbModule/com/jsf/entities/User.java
@@ -0,0 +1,76 @@
+package com.jsf.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the users database table.
+ * 
+ */
+@Entity
+@Table(name="users")
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_users")
+	private Integer idUsers;
+
+	private String login;
+
+	private String mail;
+
+	private String password;
+
+	private String role;
+
+
+	public User() {
+	}
+
+	public Integer getIdUsers() {
+		return this.idUsers;
+	}
+
+	public void setIdUsers(int idUsers) {
+		this.idUsers = idUsers;
+	}
+
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMail() {
+		return this.mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+}
